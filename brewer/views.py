171522196569942f0fc4ab0328720f5ee5a434cc
Qingyuan Zhang <miccrun@gmail.com>
2013-11-03 10:46:14 -0600

    rename some module

--- brewer/views.py
+++ brewer/views.py
@@ -1,8 +1,5 @@
 
-import datetime
-
 from django.contrib.auth.models import User
-from django.core.mail import EmailMessage
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.views.generic import (
@@ -13,11 +10,11 @@ from django.views.generic import (
 )
 
 from auth.views import LoginRequiredMixin
-import dashboard as dashboard_constants
-from dashboard.forms import (
+import brewer as brewer_constants
+from brewer.forms import (
     CreateTopicForm,CreatePostForm,
 )
-from dashboard.models import (
+from brewer.models import (
     Course,
     CourseSchedule,
     Membership,
@@ -27,22 +24,23 @@ from dashboard.models import (
 
 
 class CourseView(ListView):
-    template_name = 'dashboard/courses.html'
+    template_name = 'brewer/courses.html'
     paginate_by = 20
 
     def get_queryset(self):
         course_name = self.request.GET.get("course_name", "")
         return Course.objects.filter(
             name__icontains=course_name,
-        ) 
+        )
 
     def get_context_data(self, **kwargs):
         context = super(CourseView, self).get_context_data(**kwargs)
         context['course_name'] = self.request.GET.get("course_name", "")
         return context
-        
+
+
 class UserProfileView(LoginRequiredMixin, ListView):
-    template_name = 'dashboard/user_profile.html'
+    template_name = 'brewer/user_profile.html'
     paginate_by = 20
 
     def get_queryset(self):
@@ -66,13 +64,13 @@ class JoinClassView(LoginRequiredMixin, View):
             member=user,
             course_id=course_id,
         )
-        membership.status = dashboard_constants.ENROLL_COURSE
+        membership.status = brewer_constants.ENROLL_COURSE
         membership.save()
         return HttpResponseRedirect('/topics/' + course_id)
 
 
 class CreateTopicView(LoginRequiredMixin, CreateView):
-    template_name = 'dashboard/create_topic.html'
+    template_name = 'brewer/create_topic.html'
     model = Topic
     form_class = CreateTopicForm
 
@@ -91,29 +89,9 @@ class CreateTopicView(LoginRequiredMixin, CreateView):
         context['course_id'] = self.kwargs['course_id']
         return context
 
-class CreatePostView(LoginRequiredMixin, CreateView):
-    template_name = 'dashboard/posts.html'
-    model = Post
-    form_class = CreatePostForm
-
-    def get_success_url(self):
-        return '/posts/' + self.kwargs['topic_id']
-
-    def get_form_kwargs(self):
-        kwargs = super(CreatePostView, self).get_form_kwargs()
-        kwargs['topic_id'] = self.kwargs['topic_id']
-        kwargs['author'] = self.request.user
-        return kwargs
-
-    def get_context_data(self, **kwargs):
-        context = super(CreatePostView, self).get_context_data(**kwargs)
-        context['topic_id'] = self.kwargs['topic_id']
-        context['topic'] = Topic.objects.get(pk=self.kwargs['topic_id'])
-        context['reply'] = Post.objects.filter(topic_id=self.kwargs['topic_id'])
-        return context
 
 class TopicsView(ListView):
-    template_name = 'dashboard/topics.html'
+    template_name = 'brewer/topics.html'
     paginate_by = 20
     context_object_name = 'topics'
 
@@ -134,7 +112,7 @@ class TopicsView(ListView):
             count = Membership.objects.filter(
                 member=user,
                 course_id=course_id,
-                status=dashboard_constants.ENROLL_COURSE
+                status=brewer_constants.ENROLL_COURSE
             ).count()
             return count >= 1
         else:
@@ -145,75 +123,3 @@ class TopicsView(ListView):
         context['course'] = self.get_course()
         context['course_joined'] = self.is_course_joined()
         return context
-
-
-class PostsView(ListView):
-    template_name = 'dashboard/posts.html'
-    paginate_by = 20
-    context_object_name = 'reply'
-
-    def get_queryset(self):
-        topic_id = self.kwargs['topic_id']
-        return Post.objects.filter(
-            topic_id=topic_id,
-        )
-
-    def get_topic(self):
-        topic_id = self.kwargs['topic_id']
-        return Topic.objects.get(pk=topic_id)
-
-    def get_context_data(self, **kwargs):
-        context = super(PostsView, self).get_context_data(**kwargs)
-        context['topic'] = self.get_topic()
-        return context
-
-
-class NotificationView(View):
-    http_method_names = ['post']
-
-    def post(self, request, *args, **kwargs):
-        token = request.POST.get("token", "")
-        if token == "0lI3#5atxifYmbjuVm#0":
-
-            weekday = datetime.datetime.today().weekday() + 1
-            current = datetime.datetime.now()
-            #weekday = 2
-            #current = datetime.datetime(2012, 11, 22, 12, 20, 00)
-            time_window_left = current + datetime.timedelta(minutes=-2)
-            time_window_right = current + datetime.timedelta(minutes=2)
-
-            course_list = CourseSchedule.objects.filter(
-                end_time__gte=time_window_left,
-                end_time__lte=time_window_right,
-                weekday=weekday,
-            )
-
-            admin = User.objects.get(username='admin')
-
-            for course in course_list:
-                memberships = Membership.objects.filter(
-                    course=course.course,
-                    status=dashboard_constants.ENROLL_COURSE,
-                )
-                recipient_list = [membership.member.email for membership in memberships]
-
-                new_topic = Topic(
-                    title="How is today's class?",
-                    content="Have some discussions about today's class!",
-                    author=admin,
-                    course=course.course,
-                )
-                new_topic.save()
-
-                email = EmailMessage(
-                    "How is today's class?",
-                    'Discuss at http://192.168.42.2/posts/' + str(new_topic.id),
-                    'Study Together <noreply@micbase.com>',
-                    recipient_list,
-                    bcc=['394purple@googlegroups.com', ],
-                )
-                email.send()
-
-            return HttpResponse("OK")
-        else:
-            return HttpResponse("ERROR")
--- urls.py
+++ urls.py
@@ -1,24 +1,22 @@
 from django.conf.urls import patterns, url
 from django.conf import settings
 
-import dashboard.views
+import brewer.views
 import auth.views
 
 urlpatterns = patterns('',
-    url(r'^$', dashboard.views.CourseView.as_view(), name='course_list'),
-    url(r'^user_profile$', dashboard.views.UserProfileView.as_view(), name='user_profile'),
+    url(r'^$', brewer.views.CourseView.as_view(), name='course_list'),
+    url(r'^user_profile$', brewer.views.UserProfileView.as_view(), name='user_profile'),
 
-    url(r'^topics/(?P<course_id>\d+)$', dashboard.views.TopicsView.as_view(), name='topics'),
-    url(r'^create_topic/(?P<course_id>\d+)$', dashboard.views.CreateTopicView.as_view(), name='create_topic'),
+    url(r'^topics/(?P<course_id>\d+)$', brewer.views.TopicsView.as_view(), name='topics'),
+    url(r'^create_topic/(?P<course_id>\d+)$', brewer.views.CreateTopicView.as_view(), name='create_topic'),
 
-    url(r'^posts/(?P<topic_id>\d+)$', dashboard.views.CreatePostView.as_view(), name='group-info'),
-    url(r'^post_content/(?P<topic_id>\d+)$', dashboard.views.CreatePostView.as_view(), name='group-info'),
-    url(r'^join_course$', dashboard.views.JoinClassView.as_view(), name='join_course'),
+    url(r'^posts/(?P<topic_id>\d+)$', brewer.views.CreatePostView.as_view(), name='group-info'),
+    url(r'^post_content/(?P<topic_id>\d+)$', brewer.views.CreatePostView.as_view(), name='group-info'),
+    url(r'^join_course$', brewer.views.JoinClassView.as_view(), name='join_course'),
     url(r'^login$', auth.views.LoginView.as_view(), name='login'),
     url(r'^logout$', auth.views.LogoutView.as_view(), name='logout'),
     url(r'^register$', auth.views.RegisterView.as_view(), name='register'),
-
-    url(r'^send_notification$', dashboard.views.NotificationView.as_view(), name='send_notification'),
 )
 
 if settings.DEBUG:
