57f5c5e7cdc7c67afb7dacda5d2f3c2c1f139e44
Michael Chang <michael@micbase.com>
2013-11-03 10:46:14 -0600

    rename some module

--- settings/__init__.py
+++ settings/__init__.py
@@ -120,7 +120,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'dashboard',
+    'brewer',
 )
 
 # A sample logging configuration. The only tangible logging
