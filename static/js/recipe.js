dcc589dbf0779972e8ef4da939b05965b6bc2228
Qingyuan Zhang <miccrun@gmail.com>
2013-11-04 12:33:52 -0600

    Be able to insert new note

--- static/js/recipe.js
+++ static/js/recipe.js
@@ -1,12 +1,31 @@
+jQuery.fn.exists = function(){return this.length>0;}
+
 function save_ingredient_note(id) {
     var new_note = $("#ingredient_note_input_" + id).val();
+
+    var tag_id = '#ingredient_tag_' + id;
+    var note_id = "#ingredient_note_" + id;
+    var form_id = "#ingredient_form_" + id;
     $.post("/", { note: new_note });
-    $("#ingredient_note_" + id).show();
-    $("#ingredient_form_" + id).hide();
+
+    // TODO: put in the post success function
+
+    if (! $(note_id).exists()) {
+        new_note_tag = '<div style="display: none" id="ingredient_note_' + id +
+            '" class="ingredient_note alert alert-warning">' + new_note + '</div>'
+        $(tag_id).after(new_note_tag);
+    }
+
+    $(note_id).fadeIn();
+    $(form_id).hide();
+
+    // *****
 }
 
+$(".ingredient_form").hide();
+
 $(document).ready(function(){
-    $(".ingredient_note").click(function(){
+    $(document).on('click', '.ingredient_note', function() {
         var note_id = '#' + $(this).attr('id');
         var form_id = '#' + $(this).attr('id').replace("note", "form");
         var input_id = '#' + $(this).attr('id').replace("note", "note_input");
@@ -16,8 +35,19 @@ $(document).ready(function(){
             $(note_id).hide();
             $(input_id).val(data);
 
-            $(form_id).removeClass("hidden");
-            $(form_id).show();
+            $(form_id).fadeIn();
         });
+    });
+
+    $(".ingredient").click(function(){
+        var note_id = '#' + $(this).attr('id').replace("tag", "note");
+
+        if (! $(note_id).exists()) {
+            var form_id = '#' + $(this).attr('id').replace("tag", "form");
+            var input_id = '#' + $(this).attr('id').replace("tag", "note_input");
+
+            $(input_id).val("");
+            $(form_id).fadeIn();
+        }
     });
 }); 
