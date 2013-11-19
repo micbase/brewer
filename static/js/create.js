2f9f7200eb713966289bd44ddeaa144946c75b30
Michael Chang <michael@micbase.com>
2013-11-19 14:56:26 -0600

    Switch the creating recipe style to Bootstrap

--- static/js/create.js
+++ static/js/create.js
@@ -1,39 +1,75 @@
 jQuery.fn.exists = function(){return this.length>0;}
 
 $(function() {
-    n = 1;
+    ingredient_index = 1;
+    procedure_index = 1;
+
     $("#id_add_ingredient_btn").click(function(){
 
-        $("#edit_ingredient_inputbox").append('<div class="border-bottom-2 cf" id="ingredient_'+n+'"><ul class="cf"><li ><input class="form-control" name="categoryname" id="categoryname_'+n+'" type="text" placeholder="Category" value=""/></li><li ><input class="form-control" name="amount" id="amount_'+n+'" type="text" placeholder="Amount" value=""/></li><li ><select class="form-control" name="unit" id="unit_'+n+'"><option selected="selected" value="lbs">lbs</option><option value="gal">gal</option><option value="oz">oz</option></select></li ><li ><input class="form-control" name="ingredientname" id="ingredientname_'+n+'" type="text" placeholder="Name" value=""/></li></ul></div>');
-        n = n+1;
+        $("#ingredient_container").append('<div id="ingredient_' + ingredient_index + '">\
+            <hr>\
+            <div class="form-group">\
+                <input class="form-control" name="category" id="category_' +
+                    ingredient_index + '" type="text" placeholder="Category">\
+            </div>\
+            <div class="form-group">\
+                <input class="form-control" name="amount" id="amount_' + ingredient_index +
+                    '" type="text" placeholder="Amount">\
+            </div>\
+            <div class="form-group">\
+                <select class="form-control" name="unit" id="unit_' + ingredient_index + '">\
+                    <option selected="selected" value="lbs">lbs</option>\
+                    <option value="gal">gal</option>\
+                    <option value="oz">oz</option>\
+                </select>\
+            </div>\
+            <div class="form-group">\
+                <input class="form-control" name="ingredient_name" id="ingredient_name_'
+                + ingredient_index + '" type="text" placeholder="Name">\
+            </div>\
+        </div>');
+
+        ingredient_index++;
         $("#id_delete_ingredient_btn").show();
     });
 
     $("#id_delete_ingredient_btn").click(function(){
-        n = n-1;
-        if (n>1){
-            $("#ingredient_"+n).remove();
-        }else if (n==1){
-            $("#ingredient_"+n).remove();
+        ingredient_index--;
+        if (ingredient_index > 1){
+            $("#ingredient_" + n).remove();
+        } else if (ingredient_index == 1){
+            $("#ingredient_" + ingredient_index).remove();
             $("#id_delete_ingredient_btn").hide();
         }
-
     });
 
-    i = 1;
-    $("#id_add_it_procedure_btn").click(function(){
+    $("#id_add_procedure_btn").click(function() {
+        $("#procedure_container").append('<div id="procedure_' + procedure_index + '">\
+            <hr>\
+            <div class="form-group">\
+                <input class="form-control" name="title" id="title_' +
+                    procedure_index + '" type="text" placeholder="Title">\
+            </div>\
+            <div class="form-group">\
+                <input class="form-control" name="tag" id="tag_' + procedure_index +
+                    '" type="text" placeholder="Tag">\
+            </div>\
+            <div class="form-group">\
+                <input class="form-control" name="content" id="content_' +
+                    procedure_index + '" type="text" placeholder="Content">\
+            </div>\
+        </div>');
 
-        $("#edit_it_procedure_inputbox").append('<div class="border-bottom-2 cf" id="procedure_'+i+'"><ul class="cf"><li ><input class="form-control" name="titlename" id="titlename_'+i+'" type="text" placeholder="Title" value=""/></li><li ><input class="form-control" name="tag" id="tag_'+i+'" type="text" placeholder="Tag" value=""/></li><li ><input class="form-control" name="content" id="content_'+i+'" type="text" placeholder="Content"></li></ul></div>');
-        i = i+1;
+        procedure_index++;
         $("#id_delete_procedure_btn").show();
     });
 
-    $("#id_delete_procedure_btn").click(function(){
-        i = i-1;
-        if (i>1){
-            $("#procedure_"+i).remove();
-        }else if (i==1){
-            $("#procedure_"+i).remove();
+    $("#id_delete_procedure_btn").click(function() {
+        procedure_index--;
+        if (procedure_index > 1) {
+            $("#procedure_" + procedure_index).remove();
+        } else if (procedure_index == 1) {
+            $("#procedure_" + procedure_index).remove();
             $("#id_delete_procedure_btn").hide();
         }
     });
@@ -43,21 +79,21 @@ $(function() {
         var all_category = '';
         var all_amount = '';
         var all_unit = '';
-        var all_ingredientname = '';
+        var all_ingredient_name = '';
 
         while (true) {
-            var category_id = '#categoryname_' + id;
+            var category_id = '#category_' + id;
             var amount_id = '#amount_' + id;
             var unit_id = '#unit_' + id;
-            var ingredientname_id = '#ingredientname_' + id;
+            var ingredient_name_id = '#ingredient_name_' + id;
 
             if ($(category_id).exists() && $(amount_id).exists() &&
-                $(unit_id).exists() && $(ingredientname_id).exists()) {
+                $(unit_id).exists() && $(ingredient_name_id).exists()) {
 
                 all_category += $(category_id).val() + ',';
                 all_amount += $(amount_id).val() + ',';
                 all_unit += $(unit_id).val() + ',';
-                all_ingredientname += $(ingredientname_id).val() + ',';
+                all_ingredient_name += $(ingredient_name_id).val() + ',';
 
                 id++;
             }
@@ -68,7 +104,7 @@ $(function() {
         all_category = all_category.slice(0, -1);
         all_amount = all_amount.slice(0, -1);
         all_unit = all_unit.slice(0, -1);
-        all_ingredientname = all_ingredientname.slice(0, -1);
+        all_ingredient_name = all_ingredient_name.slice(0, -1);
 
         id = 0;
         var all_title = '';
@@ -76,7 +112,7 @@ $(function() {
         var all_content = '';
 
         while (true) {
-            var title_id = '#titlename_' + id;
+            var title_id = '#title_' + id;
             var tag_id = '#tag_' + id;
             var content_id = '#content_' + id;
 
@@ -98,8 +134,8 @@ $(function() {
         all_content = all_content.slice(0, -1);
 
         $.post("/create_recipe", {
-            recipe_name: $('#recipename').val(),
-            source_name: all_ingredientname,
+            recipe_name: $('#recipe_name').val(),
+            source_name: all_ingredient_name,
             source_variety: all_category,
             amount: all_amount,
             unit: all_unit,
